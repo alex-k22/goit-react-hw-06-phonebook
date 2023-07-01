fe953fc7f8308b23eb6a3073b6c25da3da790c55
alex-k22 <114990357+alex-k22@users.noreply.github.com>
2023-07-01 13:43:22 +0300

    mentor comment

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -1,24 +1,29 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addFilter } from 'redux/filterSlice';
 import css from '../Form/Form.module.css';
 
 const FilterContacts = () => {
   const dispatch = useDispatch();
+  const filterValue = useSelector(state => state.filter);
 
   const onChange = event => {
     dispatch(addFilter(event.target.value));
   };
+  return (
     <>
-      <p>Find contacts by name</p>
-      <input
-        className={css.input}
-        type="text"
-        name="filter"
-        title="Write you request here"
-        required
-        onChange={onChange}
-      />
-    </>
+    <p>Find contacts by name</p>
+    <input
+      className={css.input}
+      type="text"
+      name="filter"
+      title="Write you request here"
+      required
+      value={filterValue}
+      onChange={onChange}
+    />
+  </>
+  )
+
 };
 
 
